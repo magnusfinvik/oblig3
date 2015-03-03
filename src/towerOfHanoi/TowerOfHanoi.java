19feb8fe2d225055398858b8a8ba0ad4e8985e79
Magnus Finvik <magnusfinvik@hotmail.com>
2015-03-03 12:12:07 +0100

    V.0.2 added output of number of moves

--- src/towerOfHanoi/TowerOfHanoi.java
+++ src/towerOfHanoi/TowerOfHanoi.java
@@ -3,13 +3,16 @@ package towerOfHanoi;
 import java.util.Scanner;
 
 public class TowerOfHanoi {
+	static int numberOfMoves = 0;
 	public static void main(String[] args){
 		Scanner input = new Scanner(System.in);
 		System.out.print("skriv inn antall disker: ");
 		int numberOfDisks = input.nextInt();
+		numberOfMoves = ((int) Math.pow(2, numberOfDisks)-1);
 
 		System.out.println("trekkene er:");
 		moveDisks(numberOfDisks, 'A', 'B', 'C');
+		System.out.println("antall trekk er: " + numberOfMoves);
 	}
 
 	private static void moveDisks(int n, char fromTower, char toTower, char helperTower) {
@@ -20,6 +23,7 @@ public class TowerOfHanoi {
 			moveDisks(n-1, fromTower, helperTower, toTower);
 			System.out.println("Flytt disk " + n + " fra " + fromTower + " til " + toTower);
 			moveDisks(n-1, helperTower, toTower, fromTower);
+			
 		}
 	}
 }
